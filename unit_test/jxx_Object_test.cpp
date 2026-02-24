e94cd4c41d0fdb9da68153adba03671510a08daa
shumatjg <james.shumate@otis.com>
2026-02-24 11:08:56 -0500

    added git ignore
    added git ignore

---
+++ unit_test/jxx_Object_test.cpp
@@ -0,0 +1,91 @@
+#include <gtest/gtest.h>
+#include "jxx.h"
+
+class TestInteger : public jxx::lang::Cloneable<TestInteger> {
+    int value_{ 0 };
+public:
+    static bool _destructorCalled;
+
+    explicit TestInteger(int v) : value_(v) {
+        TestInteger::_destructorCalled = false;
+    }
+
+    virtual ~TestInteger() {
+        TestInteger::_destructorCalled = true;
+    }
+
+    int intValue() const noexcept { return value_; }
+
+    bool equals(const jxx::lang::Object& other) const noexcept override {
+        auto* o = dynamic_cast<const TestInteger*>(&other);
+        return o && value_ == o->value_;
+    }
+    std::size_t hashCode() const noexcept override {
+        return std::hash<int>{}(value_);
+    }
+    std::string toString() const override {
+        return std::to_string(value_);
+    }
+};
+
+bool TestInteger::_destructorCalled = false;
+
+class TestIntegerTest : public testing::Test {
+protected:
+    // You can remove any or all of the following functions if their bodies would
+    // be empty.
+
+    TestIntegerTest() {
+        // You can do set-up work for each test here.
+    }
+
+    ~TestIntegerTest() override {
+        // You can do clean-up work that doesn't throw exceptions here.
+    }
+
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
+
+    void SetUp() override {
+        TestInteger::_destructorCalled = false;
+      }
+
+    void TearDown() override {
+     }
+
+    // Class members declared here can be used by all tests in the test suite
+    // for Foo.
+};
+
+// Demonstrate some basic assertions.
+TEST(TestIntegerTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(TestIntegerTest, IntValueTest) {
+    //const std::string input_filepath = "this/package/testdata/myinputfile.dat";
+    //const std::string output_filepath = "this/package/testdata/myoutputfile.dat";
+    int val = 1;
+    auto ixx = NEW(TestInteger, val);
+    EXPECT_EQ(val, ixx->intValue());
+    ASSERT_EQ(TestInteger::_destructorCalled, false);
+    ixx.reset(); // only one reference counter, should be 0, delete object
+    ASSERT_EQ(TestInteger::_destructorCalled, true);
+}
+
+TEST(TestIntegerTest, IntValueTestScope) {
+    //const std::string input_filepath = "this/package/testdata/myinputfile.dat";
+    //const std::string output_filepath = "this/package/testdata/myoutputfile.dat";
+    int val = 5;
+    
+    {
+        auto ixx = NEW(TestInteger, val);
+        EXPECT_EQ(val, ixx->intValue());
+        ASSERT_EQ(TestInteger::_destructorCalled, false);
+    }
+
+    ASSERT_EQ(TestInteger::_destructorCalled, true);
+}
