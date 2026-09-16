a44e9e0151740532c15c9c30a8676f58e51e30eb
jamess <james.shumate@otis.com>
2026-09-16 11:09:56 -0400

    added awt scrollbar class

---
+++ sdkgui/awt/jxx.awt.Scrollbar.cpp
@@ -0,0 +1,136 @@
+#include "awt/jxx.awt.Scrollbar.h"
+
+#include <algorithm>
+#include <limits>
+
+#include "awt/event/jxx.awt.event.AdjustmentEvent.h"
+#include "awt/event/jxx.awt.event.AdjustmentListener.h"
+#include "lang/jxx.lang.IllegalArgumentException.h"
+
+namespace jxx::awt
+{
+    Scrollbar::Scrollbar() : Scrollbar(VERTICAL, 0, 10, 0, 100) {}
+
+    Scrollbar::Scrollbar(::jxx::lang::jint orientation)
+        : Scrollbar(orientation, 0, 10, 0, 100) {}
+
+    Scrollbar::Scrollbar(::jxx::lang::jint orientation,
+        ::jxx::lang::jint value,
+        ::jxx::lang::jint visibleAmount,
+        ::jxx::lang::jint minimum,
+        ::jxx::lang::jint maximum)
+        : orientation_(orientation), value_(0), visibleAmount_(1),
+          minimum_(0), maximum_(100), unitIncrement_(1),
+          blockIncrement_(10), valueIsAdjusting_(false)
+    {
+        if (orientation != HORIZONTAL && orientation != VERTICAL)
+            throw ::jxx::lang::IllegalArgumentException("orientation");
+        normalizeValues(value, visibleAmount, minimum, maximum);
+    }
+
+    ::jxx::lang::jint Scrollbar::getOrientation() const { return orientation_; }
+
+    void Scrollbar::normalizeValues(::jxx::lang::jint value,
+        ::jxx::lang::jint visibleAmount,
+        ::jxx::lang::jint minimum,
+        ::jxx::lang::jint maximum)
+    {
+        if (maximum <= minimum)
+            maximum = minimum == std::numeric_limits<::jxx::lang::jint>::max()
+                ? minimum : minimum + 1;
+        visibleAmount = std::max<::jxx::lang::jint>(1, visibleAmount);
+        visibleAmount = std::min(visibleAmount, maximum - minimum);
+        value = std::max(minimum, value);
+        value = std::min(value, maximum - visibleAmount);
+        minimum_ = minimum;
+        maximum_ = maximum;
+        visibleAmount_ = visibleAmount;
+        value_ = value;
+    }
+
+    void Scrollbar::setValues(::jxx::lang::jint value,
+        ::jxx::lang::jint visibleAmount,
+        ::jxx::lang::jint minimum,
+        ::jxx::lang::jint maximum)
+    {
+        normalizeValues(value, visibleAmount, minimum, maximum);
+        invalidate();
+    }
+
+    ::jxx::lang::jint Scrollbar::getValue() const { return value_; }
+    void Scrollbar::setValue(::jxx::lang::jint value)
+    {
+        normalizeValues(value, visibleAmount_, minimum_, maximum_);
+    }
+    ::jxx::lang::jint Scrollbar::getMinimum() const { return minimum_; }
+    void Scrollbar::setMinimum(::jxx::lang::jint minimum)
+    {
+        normalizeValues(value_, visibleAmount_, minimum, maximum_);
+    }
+    ::jxx::lang::jint Scrollbar::getMaximum() const { return maximum_; }
+    void Scrollbar::setMaximum(::jxx::lang::jint maximum)
+    {
+        normalizeValues(value_, visibleAmount_, minimum_, maximum);
+    }
+    ::jxx::lang::jint Scrollbar::getVisibleAmount() const { return visibleAmount_; }
+    void Scrollbar::setVisibleAmount(::jxx::lang::jint amount)
+    {
+        normalizeValues(value_, amount, minimum_, maximum_);
+    }
+    ::jxx::lang::jint Scrollbar::getUnitIncrement() const { return unitIncrement_; }
+    void Scrollbar::setUnitIncrement(::jxx::lang::jint increment)
+    {
+        unitIncrement_ = std::max<::jxx::lang::jint>(1, increment);
+    }
+    ::jxx::lang::jint Scrollbar::getLineIncrement() const { return getUnitIncrement(); }
+    void Scrollbar::setLineIncrement(::jxx::lang::jint increment) { setUnitIncrement(increment); }
+    ::jxx::lang::jint Scrollbar::getBlockIncrement() const { return blockIncrement_; }
+    void Scrollbar::setBlockIncrement(::jxx::lang::jint increment)
+    {
+        blockIncrement_ = std::max<::jxx::lang::jint>(1, increment);
+    }
+    ::jxx::lang::jint Scrollbar::getPageIncrement() const { return getBlockIncrement(); }
+    void Scrollbar::setPageIncrement(::jxx::lang::jint increment) { setBlockIncrement(increment); }
+    ::jxx::lang::jbool Scrollbar::getValueIsAdjusting() const { return valueIsAdjusting_; }
+    void Scrollbar::setValueIsAdjusting(::jxx::lang::jbool adjusting)
+    {
+        valueIsAdjusting_ = adjusting;
+    }
+
+    void Scrollbar::addAdjustmentListener(
+        const ::jxx::Ptr<::jxx::awt::event::AdjustmentListener>& listener)
+    {
+        if (listener != nullptr &&
+            std::find(listeners_.begin(), listeners_.end(), listener) == listeners_.end())
+            listeners_.push_back(listener);
+    }
+
+    void Scrollbar::removeAdjustmentListener(
+        const ::jxx::Ptr<::jxx::awt::event::AdjustmentListener>& listener)
+    {
+        listeners_.erase(std::remove(listeners_.begin(), listeners_.end(), listener),
+            listeners_.end());
+    }
+
+    void Scrollbar::setValueFromNative(::jxx::lang::jint value,
+        ::jxx::lang::jint adjustmentType,
+        ::jxx::lang::jbool adjusting)
+    {
+        const auto previous = value_;
+        normalizeValues(value, visibleAmount_, minimum_, maximum_);
+        valueIsAdjusting_ = adjusting;
+        if (previous == value_) return;
+        processAdjustmentEvent(::jxx::NEW<::jxx::awt::event::AdjustmentEvent>(
+            ::jxx::CAST<Adjustable>(thisPtr()),
+            ::jxx::awt::event::AdjustmentEvent::ADJUSTMENT_VALUE_CHANGED,
+            adjustmentType, value_, valueIsAdjusting_));
+    }
+
+    void Scrollbar::processAdjustmentEvent(
+        const ::jxx::Ptr<::jxx::awt::event::AdjustmentEvent>& event)
+    {
+        const auto listeners = listeners_;
+        for (const auto& listener : listeners)
+            if (listener != nullptr) listener->adjustmentValueChanged(event);
+    }
+}
---
+++ sdkgui/awt/jxx.awt.Scrollbar.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <vector>
+
+#include "awt/jxx.awt.Adjustable.h"
+#include "awt/jxx.awt.Component.h"
+
+namespace jxx::awt::event
+{
+    class AdjustmentEvent;
+    class AdjustmentListener;
+}
+
+namespace jxx::awt
+{
+    class Scrollbar :
+        public ::jxx::lang::ClassBase<Scrollbar, Component, Adjustable>
+    {
+    public:
+        using JxxSuper = Component;
+        using Super = ::jxx::lang::ClassBase<Scrollbar,
+            JxxSuper, Adjustable>;
+
+        static constexpr ::jxx::lang::jint HORIZONTAL = Adjustable::HORIZONTAL;
+        static constexpr ::jxx::lang::jint VERTICAL = Adjustable::VERTICAL;
+
+        Scrollbar();
+        explicit Scrollbar(::jxx::lang::jint orientation);
+        Scrollbar(::jxx::lang::jint orientation,
+            ::jxx::lang::jint value,
+            ::jxx::lang::jint visibleAmount,
+            ::jxx::lang::jint minimum,
+            ::jxx::lang::jint maximum);
+        ~Scrollbar() override = default;
+
+        ::jxx::lang::jint getOrientation() const override;
+        void setValues(::jxx::lang::jint value,
+            ::jxx::lang::jint visibleAmount,
+            ::jxx::lang::jint minimum,
+            ::jxx::lang::jint maximum);
+        ::jxx::lang::jint getValue() const override;
+        void setValue(::jxx::lang::jint value) override;
+        ::jxx::lang::jint getMinimum() const override;
+        void setMinimum(::jxx::lang::jint minimum) override;
+        ::jxx::lang::jint getMaximum() const override;
+        void setMaximum(::jxx::lang::jint maximum) override;
+        ::jxx::lang::jint getVisibleAmount() const override;
+        void setVisibleAmount(::jxx::lang::jint amount) override;
+        ::jxx::lang::jint getUnitIncrement() const override;
+        void setUnitIncrement(::jxx::lang::jint increment) override;
+        ::jxx::lang::jint getLineIncrement() const;
+        void setLineIncrement(::jxx::lang::jint increment);
+        ::jxx::lang::jint getBlockIncrement() const override;
+        void setBlockIncrement(::jxx::lang::jint increment) override;
+        ::jxx::lang::jint getPageIncrement() const;
+        void setPageIncrement(::jxx::lang::jint increment);
+        ::jxx::lang::jbool getValueIsAdjusting() const;
+        void setValueIsAdjusting(::jxx::lang::jbool adjusting);
+
+        void addAdjustmentListener(
+            const ::jxx::Ptr<::jxx::awt::event::AdjustmentListener>& listener) override;
+        void removeAdjustmentListener(
+            const ::jxx::Ptr<::jxx::awt::event::AdjustmentListener>& listener) override;
+
+    protected:
+        virtual void processAdjustmentEvent(
+            const ::jxx::Ptr<::jxx::awt::event::AdjustmentEvent>& event);
+
+    private:
+        void setValueFromNative(::jxx::lang::jint value,
+            ::jxx::lang::jint adjustmentType,
+            ::jxx::lang::jbool adjusting);
+        void normalizeValues(::jxx::lang::jint value,
+            ::jxx::lang::jint visibleAmount,
+            ::jxx::lang::jint minimum,
+            ::jxx::lang::jint maximum);
+
+        ::jxx::lang::jint orientation_;
+        ::jxx::lang::jint value_;
+        ::jxx::lang::jint visibleAmount_;
+        ::jxx::lang::jint minimum_;
+        ::jxx::lang::jint maximum_;
+        ::jxx::lang::jint unitIncrement_;
+        ::jxx::lang::jint blockIncrement_;
+        ::jxx::lang::jbool valueIsAdjusting_;
+        std::vector<::jxx::Ptr<::jxx::awt::event::AdjustmentListener>> listeners_;
+    };
+}
