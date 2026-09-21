0bf4b9faea5eb2475db477223e21485466ed7bde
jamess <james.shumate@otis.com>
2026-09-21 14:58:31 -0400

    printer io exceptions

---
+++ sdkgui/awt/print/jxx.awt.print.PrinterIOException.cpp
@@ -0,0 +1,25 @@
+#include "awt/print/jxx.awt.print.PrinterIOException.h"
+
+#include "io/jxx.io.IOException.h"
+
+namespace jxx::awt::print
+{
+    PrinterIOException::PrinterIOException(
+        const ::jxx::Ptr<::jxx::io::IOException>& exception)
+        : Super(exception == nullptr ? nullptr : exception->toString()),
+          exception_(exception)
+    {
+    }
+
+    ::jxx::Ptr<::jxx::io::IOException>
+    PrinterIOException::getIOException() const
+    {
+        return exception_;
+    }
+
+    ::jxx::Ptr<::jxx::lang::Throwable>
+    PrinterIOException::getCause() const
+    {
+        return exception_;
+    }
+}
---
+++ sdkgui/awt/print/jxx.awt.print.PrinterIOException.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "awt/print/jxx.awt.print.PrinterException.h"
+
+namespace jxx::io { class IOException; }
+
+namespace jxx::awt::print
+{
+    class PrinterIOException : public ::jxx::lang::ClassBase<
+        PrinterIOException, PrinterException>
+    {
+    public:
+        using JxxSuper = PrinterException;
+        using Super = ::jxx::lang::ClassBase<PrinterIOException, JxxSuper>;
+
+        explicit PrinterIOException(
+            const ::jxx::Ptr<::jxx::io::IOException>& exception);
+        ::jxx::Ptr<::jxx::io::IOException> getIOException() const;
+        ::jxx::Ptr<::jxx::lang::Throwable> getCause() const override;
+
+    private:
+        ::jxx::Ptr<::jxx::io::IOException> exception_;
+    };
+}
