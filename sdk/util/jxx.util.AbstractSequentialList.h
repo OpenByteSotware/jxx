664da4760e30428a4147a60b6256d87c881990b7
jamess <james.shumate@otis.com>
2026-09-23 05:16:39 -0400

    updated abstract list

--- sdk/util/jxx.util.AbstractSequentialList.h
+++ sdk/util/jxx.util.AbstractSequentialList.h
@@ -6,8 +6,8 @@ class AbstractSequentialList : public AbstractList<E> {
 public:
     virtual ~AbstractSequentialList() = default;
     jxx::Ptr<E> get(jxx::lang::jint i) const override { auto self=const_cast<AbstractSequentialList<E>*>(this); auto it=self->listIterator(i); if(!it->hasNext()) throw jxx::lang::IndexOutOfBoundsException(); return it->next(); }
-    jxx::Ptr<E> set(jxx::lang::jint i,const jxx::Ptr<E>& e) override { auto it=listIterator(i); if(!it->hasNext()) throw jxx::lang::IndexOutOfBoundsException(); auto old=it->next(); it->set(e); return old; }
-    void add(jxx::lang::jint i,const jxx::Ptr<E>& e) override { listIterator(i)->add(e); }
-    jxx::Ptr<E> remove(jxx::lang::jint i) override { auto it=listIterator(i); if(!it->hasNext()) throw jxx::lang::IndexOutOfBoundsException(); auto old=it->next(); it->remove(); return old; }
+    jxx::Ptr<E> set(jxx::lang::jint i,const jxx::Ptr<E>& e) override { auto it=this->listIterator(i); if(!it->hasNext()) throw jxx::lang::IndexOutOfBoundsException(); auto old=it->next(); it->set(e); return old; }
+    void add(jxx::lang::jint i,const jxx::Ptr<E>& e) override { this->listIterator(i)->add(e); }
+    jxx::Ptr<E> remove(jxx::lang::jint i) override { auto it=this->listIterator(i); if(!it->hasNext()) throw jxx::lang::IndexOutOfBoundsException(); auto old=it->next(); it->remove(); return old; }
 };
 } // namespace jxx::util
