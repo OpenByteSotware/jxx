d8ddc359e6a1b5d7369641c11dd6e8df0ea92f70
jamess <james.shumate@otis.com>
2026-09-26 03:57:34 -0400

    updated sni server header

--- sdk/ext/net/ssl/jxx.ext.net.ssl.SNIServerName.h
+++ sdk/ext/net/ssl/jxx.ext.net.ssl.SNIServerName.h
@@ -6,6 +6,13 @@ namespace jxx::ext::net::ssl
 {
 	class SNIServerName :public ::jxx::lang::ClassBase<SNIServerName, ::jxx::lang::Object>
 	{
-	public:using JxxSuper = ::jxx::lang::Object; using Super = ::jxx::lang::ClassBase<SNIServerName, JxxSuper>; virtual ~SNIServerName()override = default; ::jxx::lang::jint getType()const noexcept; ::jxx::lang::ByteArray getEncoded()const; protected:SNIServerName(::jxx::lang::jint type, const ::jxx::lang::ByteArray& encoded); private: ::jxx::lang::jint type_; ::jxx::lang::ByteArray encoded_;
+	public:using JxxSuper = ::jxx::lang::Object;
+	using Super = ::jxx::lang::ClassBase<SNIServerName, JxxSuper>; 
+	virtual ~SNIServerName()override = default; ::jxx::lang::jint getType()const noexcept; 
+	::jxx::lang::ByteArray getEncoded()const; 
+	protected:
+	SNIServerName(::jxx::lang::jint type, const ::jxx::lang::ByteArray& encoded); 
+	private:
+		::jxx::lang::jint type_; ::jxx::lang::ByteArray encoded_;
 	};
 }
