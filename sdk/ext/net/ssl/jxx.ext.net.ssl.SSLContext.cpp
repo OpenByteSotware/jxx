262f729479d450c9a582cadd2e6cbaff61e5eb03
jamess <james.shumate@otis.com>
2026-09-26 04:00:50 -0400

    updated ssll context

--- sdk/ext/net/ssl/jxx.ext.net.ssl.SSLContext.cpp
+++ sdk/ext/net/ssl/jxx.ext.net.ssl.SSLContext.cpp
@@ -3,56 +3,90 @@
 #include <mutex>
 
 #include "ext/net/ssl/internal/jxx.ext.net.ssl.internal.OpenSslContextConfig.h"
-#include "ext/net/ssl/internal/jxx.ext.net.ssl.internal.OpenSslSocketFactory.h"
 #include "ext/net/ssl/internal/jxx.ext.net.ssl.internal.OpenSslServerSocketFactory.h"
 #include "ext/net/ssl/internal/jxx.ext.net.ssl.internal.OpenSslSessionContext.h"
+#include "ext/net/ssl/internal/jxx.ext.net.ssl.internal.OpenSslSocketFactory.h"
+#include "ext/net/ssl/jxx.ext.net.ssl.SSLParameters.h"
 #include "lang/jxx.lang.IllegalArgumentException.h"
 #include "lang/jxx.lang.NullPointerException.h"
 #include "lang/jxx.lang.UnsupportedOperationException.h"
 #include "security/jxx.security.Provider.h"
-#include "ext/net/ssl/jxx.ext.net.ssl.SSLParameters.h"
 
 namespace jxx::ext::net::ssl {
 namespace {
+
 std::mutex defaultContextMutex;
 ::jxx::Ptr<SSLContext> defaultContext;
-}
 
-SSLContext::SSLContext(const ::jxx::Ptr<::jxx::lang::String>& protocol)
+} // namespace
+
+SSLContext::SSLContext(
+    const ::jxx::Ptr<::jxx::lang::String>& protocol)
     : protocol_(protocol)
-    , provider_(::jxx::NEW<::jxx::security::Provider>(::jxx::NEW<::jxx::lang::String>("OpenSSL")))
+    , provider_(::jxx::NEW<::jxx::security::Provider>(
+          ::jxx::NEW<::jxx::lang::String>("OpenSSL")))
+    , clientSessionContext_(
+          ::jxx::NEW<internal::OpenSslSessionContext>())
+    , serverSessionContext_(
+          ::jxx::NEW<internal::OpenSslSessionContext>())
     , config_(std::make_shared<internal::OpenSslContextConfig>(
-          protocol,
+          protocol_,
           nullptr,
           nullptr,
           nullptr,
           clientSessionContext_,
-          serverSessionContext_))
-    , clientSessionContext_(::jxx::NEW<internal::OpenSslSessionContext>())
-    , serverSessionContext_(::jxx::NEW<internal::OpenSslSessionContext>()) {
-    if (protocol_ == nullptr) throw ::jxx::lang::NullPointerException();
+          serverSessionContext_)) {
+    if (protocol_ == nullptr)
+        throw ::jxx::lang::NullPointerException();
 }
 
 SSLContext::~SSLContext() = default;
 
 ::jxx::Ptr<SSLContext> SSLContext::getInstance(
     const ::jxx::Ptr<::jxx::lang::String>& protocol) {
-    if (protocol == nullptr) throw ::jxx::lang::NullPointerException();
+    if (protocol == nullptr)
+        throw ::jxx::lang::NullPointerException();
+
     const auto value = protocol->utf8();
-    if (value != "TLS" && value != "TLSv1.2" && value != "TLSv1.3")
+    if (value != "TLS" &&
+        value != "TLSv1" &&
+        value != "TLSv1.2" &&
+        value != "TLSv1.3")
         throw ::jxx::lang::IllegalArgumentException();
+
     return ::jxx::NEW<SSLContext>(protocol);
+}
+
+::jxx::Ptr<SSLContext> SSLContext::getInstance(
+    const ::jxx::Ptr<::jxx::lang::String>& protocol,
+    const ::jxx::Ptr<::jxx::lang::String>& provider) {
+    if (provider == nullptr)
+        throw ::jxx::lang::IllegalArgumentException();
+    if (provider->utf8() != "OpenSSL")
+        throw ::jxx::lang::IllegalArgumentException();
+    return getInstance(protocol);
+}
+
+::jxx::Ptr<SSLContext> SSLContext::getInstance(
+    const ::jxx::Ptr<::jxx::lang::String>& protocol,
+    const ::jxx::Ptr<::jxx::security::Provider>& provider) {
+    if (provider == nullptr)
+        throw ::jxx::lang::IllegalArgumentException();
+    return getInstance(protocol, provider->getName());
 }
 
 ::jxx::Ptr<SSLContext> SSLContext::getDefault() {
     std::lock_guard<std::mutex> lock(defaultContextMutex);
     if (defaultContext == nullptr)
-        defaultContext = getInstance(::jxx::NEW<::jxx::lang::String>("TLS"));
+        defaultContext = getInstance(
+            ::jxx::NEW<::jxx::lang::String>("TLS"));
     return defaultContext;
 }
 
-void SSLContext::setDefault(const ::jxx::Ptr<SSLContext>& context) {
-    if (context == nullptr) throw ::jxx::lang::NullPointerException();
+void SSLContext::setDefault(
+    const ::jxx::Ptr<SSLContext>& context) {
+    if (context == nullptr)
+        throw ::jxx::lang::NullPointerException();
     std::lock_guard<std::mutex> lock(defaultContextMutex);
     defaultContext = context;
 }
@@ -74,19 +108,55 @@ void SSLContext::init(
     return ::jxx::NEW<internal::OpenSslSocketFactory>(config_);
 }
 
-::jxx::Ptr<::jxx::lang::String> SSLContext::getProtocol() const {
+::jxx::Ptr<SSLServerSocketFactory>
+SSLContext::getServerSocketFactory() {
+    return ::jxx::NEW<internal::OpenSslServerSocketFactory>(config_);
+}
+
+::jxx::Ptr<SSLSessionContext>
+SSLContext::getClientSessionContext() {
+    return clientSessionContext_;
+}
+
+::jxx::Ptr<SSLSessionContext>
+SSLContext::getServerSessionContext() {
+    return serverSessionContext_;
+}
+
+::jxx::Ptr<SSLEngine> SSLContext::createSSLEngine() {
+    throw ::jxx::lang::UnsupportedOperationException();
+}
+
+::jxx::Ptr<SSLEngine> SSLContext::createSSLEngine(
+    const ::jxx::Ptr<::jxx::lang::String>&,
+    ::jxx::lang::jint) {
+    throw ::jxx::lang::UnsupportedOperationException();
+}
+
+::jxx::Ptr<SSLParameters>
+SSLContext::getDefaultSSLParameters() {
+    const auto parameters = ::jxx::NEW<SSLParameters>();
+    const auto factory = getSocketFactory();
+    parameters->setCipherSuites(factory->getDefaultCipherSuites());
+    return parameters;
+}
+
+::jxx::Ptr<SSLParameters>
+SSLContext::getSupportedSSLParameters() {
+    const auto parameters = ::jxx::NEW<SSLParameters>();
+    const auto factory = getSocketFactory();
+    parameters->setCipherSuites(factory->getSupportedCipherSuites());
+    return parameters;
+}
+
+::jxx::Ptr<::jxx::lang::String>
+SSLContext::getProtocol() const {
     return protocol_;
 }
 
+::jxx::Ptr<::jxx::security::Provider>
+SSLContext::getProvider() const {
+    return provider_;
+}
 
-::jxx::Ptr<SSLContext> SSLContext::getInstance(const ::jxx::Ptr<::jxx::lang::String>& protocol,const ::jxx::Ptr<::jxx::lang::String>& provider){if(provider==nullptr)throw ::jxx::lang::IllegalArgumentException();if(provider->utf8()!="OpenSSL")throw ::jxx::lang::IllegalArgumentException();return getInstance(protocol);}
-::jxx::Ptr<SSLContext> SSLContext::getInstance(const ::jxx::Ptr<::jxx::lang::String>& protocol,const ::jxx::Ptr<::jxx::security::Provider>& provider){if(provider==nullptr)throw ::jxx::lang::IllegalArgumentException();return getInstance(protocol,provider->getName());}
-::jxx::Ptr<::jxx::security::Provider> SSLContext::getProvider()const{return provider_;}
-::jxx::Ptr<SSLServerSocketFactory> SSLContext::getServerSocketFactory(){return ::jxx::NEW<internal::OpenSslServerSocketFactory>(config_);}
-::jxx::Ptr<SSLSessionContext> SSLContext::getClientSessionContext(){return clientSessionContext_;}
-::jxx::Ptr<SSLSessionContext> SSLContext::getServerSessionContext(){return serverSessionContext_;}
-::jxx::Ptr<SSLEngine> SSLContext::createSSLEngine(){throw ::jxx::lang::UnsupportedOperationException();}
-::jxx::Ptr<SSLEngine> SSLContext::createSSLEngine(const ::jxx::Ptr<::jxx::lang::String>&,::jxx::lang::jint){throw ::jxx::lang::UnsupportedOperationException();}
-::jxx::Ptr<SSLParameters> SSLContext::getDefaultSSLParameters(){auto p=::jxx::NEW<SSLParameters>();auto f=getSocketFactory();p->setCipherSuites(f->getDefaultCipherSuites());return p;}
-::jxx::Ptr<SSLParameters> SSLContext::getSupportedSSLParameters(){auto p=::jxx::NEW<SSLParameters>();auto f=getSocketFactory();p->setCipherSuites(f->getSupportedCipherSuites());return p;}
 } // namespace jxx::ext::net::ssl
--- sdk/ext/net/ssl/jxx.ext.net.ssl.SSLContext.h
+++ sdk/ext/net/ssl/jxx.ext.net.ssl.SSLContext.h
@@ -1,41 +1,84 @@
 #pragma once
+
 #include <memory>
+
 #include "lang/jxx.lang.ClassInfo.h"
 #include "lang/jxx.lang.Object.h"
 #include "lang/jxx.lang.String.h"
 #include "lang/jxx.lang.buildin_array.h"
-namespace jxx::security
-{
-	class SecureRandom; class Provider;
+
+namespace jxx::security {
+class Provider;
+class SecureRandom;
 }
-namespace jxx::ext::net::ssl::internal
-{
-	class OpenSslContextConfig;
-	class OpenSslSessionContext;
+
+namespace jxx::ext::net::ssl::internal {
+class OpenSslContextConfig;
+class OpenSslSessionContext;
 }
-namespace jxx::ext::net::ssl
-{
-	class KeyManager; class TrustManager; class SSLSocketFactory; class SSLServerSocketFactory; class SSLSessionContext; class SSLEngine; class SSLParameters;
-	class SSLContext final :public ::jxx::lang::ClassBase<SSLContext, ::jxx::lang::Object>
-	{
-	public:
-		using KeyManagerArray = ::jxx::lang::JxxArray<::jxx::Ptr<KeyManager>, 1U>; using TrustManagerArray = ::jxx::lang::JxxArray<::jxx::Ptr<TrustManager>, 1U>;
-		explicit SSLContext(const ::jxx::Ptr<::jxx::lang::String>& protocol); ~SSLContext()override;
-		static ::jxx::Ptr<SSLContext> getInstance(const ::jxx::Ptr<::jxx::lang::String>& protocol);
-		static ::jxx::Ptr<SSLContext> getInstance(const ::jxx::Ptr<::jxx::lang::String>& protocol, const ::jxx::Ptr<::jxx::lang::String>& provider);
-		static ::jxx::Ptr<SSLContext> getInstance(const ::jxx::Ptr<::jxx::lang::String>& protocol, const ::jxx::Ptr<::jxx::security::Provider>& provider);
-		static ::jxx::Ptr<SSLContext> getDefault(); static void setDefault(const ::jxx::Ptr<SSLContext>& context);
-		void init(const ::jxx::Ptr<KeyManagerArray>& keyManagers, const ::jxx::Ptr<TrustManagerArray>& trustManagers, const ::jxx::Ptr<::jxx::security::SecureRandom>& secureRandom);
-		::jxx::Ptr<SSLSocketFactory> getSocketFactory(); ::jxx::Ptr<SSLServerSocketFactory> getServerSocketFactory();
-		::jxx::Ptr<SSLSessionContext> getClientSessionContext(); ::jxx::Ptr<SSLSessionContext> getServerSessionContext();
-		::jxx::Ptr<SSLEngine> createSSLEngine(); ::jxx::Ptr<SSLEngine> createSSLEngine(const ::jxx::Ptr<::jxx::lang::String>& peerHost, ::jxx::lang::jint peerPort);
-		::jxx::Ptr<SSLParameters> getDefaultSSLParameters(); ::jxx::Ptr<SSLParameters> getSupportedSSLParameters();
-		::jxx::Ptr<::jxx::lang::String> getProtocol()const; ::jxx::Ptr<::jxx::security::Provider> getProvider()const;
-		private: 
-			::jxx::Ptr<::jxx::lang::String> protocol_;
-            ::jxx::Ptr<::jxx::security::Provider> provider_;
-            std::shared_ptr<::jxx::ext::net::ssl::internal::OpenSslContextConfig> config_;
-            ::jxx::Ptr<::jxx::ext::net::ssl::internal::OpenSslSessionContext> clientSessionContext_;
-            ::jxx::Ptr<::jxx::ext::net::ssl::internal::OpenSslSessionContext> serverSessionContext_;
-	};
-}
+
+namespace jxx::ext::net::ssl {
+
+class KeyManager;
+class SSLParameters;
+class SSLServerSocketFactory;
+class SSLEngine;
+class SSLSessionContext;
+class SSLSocketFactory;
+class TrustManager;
+
+class SSLContext final
+    : public ::jxx::lang::ClassBase<
+          SSLContext,
+          ::jxx::lang::Object> {
+public:
+    using JxxSuper = ::jxx::lang::Object;
+    using Super = ::jxx::lang::ClassBase<SSLContext, JxxSuper>;
+    using KeyManagerArray = ::jxx::lang::JxxArray<
+        ::jxx::Ptr<KeyManager>, 1U>;
+    using TrustManagerArray = ::jxx::lang::JxxArray<
+        ::jxx::Ptr<TrustManager>, 1U>;
+
+    explicit SSLContext(
+        const ::jxx::Ptr<::jxx::lang::String>& protocol);
+    ~SSLContext() override;
+
+    static ::jxx::Ptr<SSLContext> getInstance(
+        const ::jxx::Ptr<::jxx::lang::String>& protocol);
+    static ::jxx::Ptr<SSLContext> getInstance(
+        const ::jxx::Ptr<::jxx::lang::String>& protocol,
+        const ::jxx::Ptr<::jxx::lang::String>& provider);
+    static ::jxx::Ptr<SSLContext> getInstance(
+        const ::jxx::Ptr<::jxx::lang::String>& protocol,
+        const ::jxx::Ptr<::jxx::security::Provider>& provider);
+
+    static ::jxx::Ptr<SSLContext> getDefault();
+    static void setDefault(const ::jxx::Ptr<SSLContext>& context);
+
+    void init(
+        const ::jxx::Ptr<KeyManagerArray>& keyManagers,
+        const ::jxx::Ptr<TrustManagerArray>& trustManagers,
+        const ::jxx::Ptr<::jxx::security::SecureRandom>& secureRandom);
+
+    ::jxx::Ptr<SSLSocketFactory> getSocketFactory();
+    ::jxx::Ptr<SSLServerSocketFactory> getServerSocketFactory();
+    ::jxx::Ptr<SSLSessionContext> getClientSessionContext();
+    ::jxx::Ptr<SSLSessionContext> getServerSessionContext();
+    ::jxx::Ptr<SSLEngine> createSSLEngine();
+    ::jxx::Ptr<SSLEngine> createSSLEngine(
+        const ::jxx::Ptr<::jxx::lang::String>& peerHost,
+        ::jxx::lang::jint peerPort);
+    ::jxx::Ptr<SSLParameters> getDefaultSSLParameters();
+    ::jxx::Ptr<SSLParameters> getSupportedSSLParameters();
+    ::jxx::Ptr<::jxx::lang::String> getProtocol() const;
+    ::jxx::Ptr<::jxx::security::Provider> getProvider() const;
+
+private:
+    ::jxx::Ptr<::jxx::lang::String> protocol_;
+    ::jxx::Ptr<::jxx::security::Provider> provider_;
+    ::jxx::Ptr<internal::OpenSslSessionContext> clientSessionContext_;
+    ::jxx::Ptr<internal::OpenSslSessionContext> serverSessionContext_;
+    std::shared_ptr<internal::OpenSslContextConfig> config_;
+};
+
+} // namespace jxx::ext::net::ssl
